1372724437c7c8d7aee5e4e90a9c98a9337a897a
grromko <106376134+grromko@users.noreply.github.com>
2022-05-27 12:39:17 +0300

    Add files via upload

---
+++ One (1).java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class One {
+    int a;
+    int b;
+
+    public One(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    void profi(){
+        int p = a+b+b+a;
+        System.out.println("Perimetr " + p);
+    }
+}
